e681fae247b20c96634c83a833ebb53d89004123
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2026-05-05 23:17:22 -0600

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let right = 0
+        let left = numbers.length - 1
+        while(right < left){
+            console.log(numbers[right])
+            console.log(numbers[left])
+            
+            if(numbers[right] + numbers[left] == target){
+                return [right + 1, left + 1]
+            }
+            else if(numbers[right] + numbers[left] > target){
+                left--
+                continue
+            }
+            else if(numbers[right] + numbers[left] < target){
+                right++
+                continue
+            }
+               
+               
+            }
+    }
+}
